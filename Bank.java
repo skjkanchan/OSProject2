00b7017903d34a0988488c499c03ccb41b970aad
skjkanchan <skjkanchan123@gmail.com>
2025-04-10 18:18:40 -0500

    Added transaction type for customers

--- Bank.java
+++ Bank.java
@@ -1,5 +1,15 @@
 
+import java.util.Random;
+
 public class Bank {
+
+    static Random random = new Random();
+
+
+    // Transaction types
+    enum TransactionType {
+        DEPOSIT, WITHDRAW, CHECK_BALANCE
+    }
 
     static class Teller extends Thread {
         int tellerId;
@@ -16,12 +26,38 @@ public class Bank {
 
     }
 
+    static class Customer extends Thread {
+        int customerId;
+        TransactionType transactionType;
+
+        public Customer(int id, TransactionType type) {
+            this.customerId = id;
+            this.transactionType = type;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Customer " + customerId + " enters the bank for transaction " + transactionType);
+        }
+
+
+    }
+
     public static void main(String args[]) { 
         Teller[] tellers = new Teller[2];
         for (int i = 0; i < 2; i++) {
             tellers[i] = new Teller(i + 1);
             tellers[i].start();
         }
+
+
+        Customer[] customers = new Customer[5];
+        for (int i = 0; i < 5; i++) {
+            TransactionType type = TransactionType.values()[random.nextInt(TransactionType.values().length)];
+            customers[i] = new Customer(i + 1, type);
+            customers[i].start();
+        }
+
 
     }
 
